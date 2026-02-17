f5cd8e8012a0cd91e155a67dea661bca27f74ba6
sungheepark <sxnghp@uw.edu>
2026-02-17 14:41:46 -0800

    iteratively reran cn algorithm

--- students/sunghee/run-cn.sh
+++ students/sunghee/run-cn.sh
@@ -1,15 +1,28 @@
 #!/bin/bash
-"""
-./run-cn.sh
-"""
+# """
+# ./run-cn.sh
+# """
+
+# Exit on error
+set -e
+
+
+if [ ! -d "communitynotes_env" ]; then
+    echo "Creating virtual environment..."
+    python3 -m venv communitynotes_env
+else
+    echo "Virtual environment already exists."
+fi
+
+# 2. Activate environment
+echo "Activating environment..."
+source communitynotes_env/bin/activate
+
+# 3. Install requirements
+echo "Installing requirements..."
+pip install -r communitynotes/requirements.txt
+
+echo "Environment setup complete."
 
 # 1. Preprocess data
-python community-notes/sourcecode/data-processing.py
-
-# 2. Run Community Notes scoring
-python community-notes/sourcecode/main.py \
-  --enrollment data/userEnrollment-00001.tsv \
-  --notes data/notes-00001.tsv \
-  --ratings data/ratings-00001.tsv \
-  --status data/noteStatusHistory-00001.tsv \
-  --outdir data
+python main.py 2026-01-31
